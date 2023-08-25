ae595b03003ab6e1d49c57013a6f06565883ed4f
latteflo <sf.simion.f@gmail.com>
2023-08-25 20:32:10 +0200

    added db on vercel

--- config/db.mjs
+++ config/db.mjs
@@ -5,12 +5,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+})
 
 pool.connect()
   .then(() => {
