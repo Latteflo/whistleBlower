42abdc5080a06f65f64ba252668eb2bcba2545c4
latteflo <sf.simion.f@gmail.com>
2023-09-01 12:00:14 +0200

    fixed import path

--- routes/reportRoutes.mjs
+++ routes/reportRoutes.mjs
@@ -4,34 +4,32 @@ import {
   getReportById,
   updateReport,
   deleteReport,
-  getReportsByPriorityColor,
   getAllReports,
   updateReportStatusController,
   getReportsByStatus,
+  updateReportMediaController,
 } from "../controllers/ReportController.mjs"
 import {
   authMiddleware,
   authMiddlewareWithRole,
 } from "../middleware/authMiddleware.mjs"
-import { updateReportMediaController } from "../controllers/ReportController.mjs";
-
+import { getReportsByPriorityColor } from "../controllers/PriorityController.mjs"
 
 const router = express.Router()
 
 // Create a new report
-router.post("/", authMiddleware, createReport);
-router.patch("/media/:id", updateReportMediaController);
-router.get("/", authMiddlewareWithRole("admin"), getAllReports);
-router.get("/:id", authMiddleware, getReportById);
-router.put("/:id", authMiddleware, updateReport);
-router.delete("/:id", authMiddleware, deleteReport);
-router.get("/priority-color/:color", authMiddlewareWithRole("admin"), getReportsByPriorityColor);
-router.put("/:id/status", authMiddleware, updateReportStatusController);
-router.get("/status", authMiddlewareWithRole("admin"), getReportsByStatus);
+router.post("/", authMiddleware, createReport)
+router.patch("/media/:id", updateReportMediaController)
+router.get("/", authMiddlewareWithRole("admin"), getAllReports)
+router.get("/:id", authMiddleware, getReportById)
+router.put("/:id", authMiddleware, updateReport)
+router.delete("/:id", authMiddleware, deleteReport)
+router.get(
+  "/priority-color/:color",
+  authMiddlewareWithRole("admin"),
+  getReportsByPriorityColor
+)
+router.put("/:id/status", authMiddleware, updateReportStatusController)
+router.get("/status", authMiddlewareWithRole("admin"), getReportsByStatus)
 
-export default router;
-
-
-
-
-
+export default router
