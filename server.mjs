2e02d62164abacb832a36a815b20135ec9768220
latteflo <sf.simion.f@gmail.com>
2023-09-01 14:01:40 +0200

    removed extra route

--- server.mjs
+++ server.mjs
@@ -8,8 +8,7 @@ import searchRoutes from "./routes/searchRoutes.mjs";
 import priorityRoutes from "./routes/priorityRoutes.mjs";
 import categoryRoutes from "./routes/categoryRoutes.mjs";
 import auditRoutes from "./routes/auditLogRoutes.mjs";
-import getClientDashboard from "./controllers/ClientController.mjs";
-import getAdminDashboard from "./controllers/AdminController.mjs";
+
 
 
 const app = express();
@@ -21,8 +20,6 @@ app.use(express.json());
 
 // Routes
 app.use("/users", userRoutes);
-app.get("/client/dashboard", getClientDashboard);
-app.get("/admin/dashboard", getAdminDashboard);
 app.use("/reports", reportRoutes);
 app.use("/replies", replyRoutes);
 app.use("/search", searchRoutes);
