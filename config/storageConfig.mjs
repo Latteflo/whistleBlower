4455030042934d8d78bc1ca0ae25c35fdb547920
latteflo <sf.simion.f@gmail.com>
2023-09-06 01:37:06 +0300

    updated storage settings

--- config/storageConfig.mjs
+++ config/storageConfig.mjs
@@ -1,35 +1,34 @@
-import cloudinary from 'cloudinary';
+import { Readable } from 'stream';
+import { v2 as cloudinary } from 'cloudinary';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 // Cloudinary configuration
-cloudinary.v2.config({
+cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true
 });
 
-// Function to upload media to Cloudinary
-export const uploadToCloudinary = async (mediaBuffer, fileName) => {
-  try {
-    const result = await cloudinary.v2.uploader.upload_stream({
-      resource_type: 'auto', 
-      public_id: fileName // Use the original file name as the public ID
+export const uploadToCloudinary = (mediaBuffer, fileName) => {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream({
+      resource_type: 'auto',
+      public_id: fileName
     }, (error, result) => {
       if (error) {
         console.error('Cloudinary upload error:', error);
-        return null;
+        reject(error);
+      } else {
+        resolve(result.secure_url);
       }
-      // Return the secure URL of the uploaded resource
-      return result.secure_url;
     });
 
-    // Create a readable stream from the buffer and pipe it to Cloudinary
-    const readableStream = new Readable().push(mediaBuffer).push(null);
-    readableStream.pipe(result);
-  } catch (error) {
-    console.error('Error uploading to Cloudinary:', error);
-    return null;
-  }
+    const readableStream = new Readable();
+    readableStream.push(mediaBuffer);
+    readableStream.push(null);
+    readableStream.pipe(uploadStream);
+  });
 };
