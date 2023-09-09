fd8316eba56b19d71011494ae2419876ec8afa66
latteflo <sf.simion.f@gmail.com>
2023-09-10 01:37:40 +0200

    added hemlet

--- server.mjs
+++ server.mjs
@@ -1,36 +1,35 @@
-import express from "express";
-import "./config/db.mjs";
-import "./config/jwt.mjs";
-import userRoutes from "./routes/userRoutes.mjs";
-import reportRoutes from "./routes/reportRoutes.mjs";
-import replyRoutes from "./routes/replyRoutes.mjs";
-import searchRoutes from "./routes/searchRoutes.mjs";
-import priorityRoutes from "./routes/priorityRoutes.mjs";
-import categoryRoutes from "./routes/categoryRoutes.mjs";
-import auditRoutes from "./routes/auditLogRoutes.mjs";
+import express from "express"
+import helmet from "helmet"
+import "./config/db.mjs"
+import "./config/jwt.mjs"
+import userRoutes from "./routes/userRoutes.mjs"
+import reportRoutes from "./routes/reportRoutes.mjs"
+import replyRoutes from "./routes/replyRoutes.mjs"
+import searchRoutes from "./routes/searchRoutes.mjs"
+import priorityRoutes from "./routes/priorityRoutes.mjs"
+import categoryRoutes from "./routes/categoryRoutes.mjs"
+import auditRoutes from "./routes/auditLogRoutes.mjs"
 
-
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
+const app = express()
+const PORT = process.env.PORT || 3000
 
 // Middleware
-app.use(express.json());
+app.use(express.json())
+app.use(helmet())
 
 // Routes
-app.use("/users", userRoutes);
-app.use("/reports", reportRoutes);
-app.use("/replies", replyRoutes);
-app.use("/search", searchRoutes);
-app.use("/priorities", priorityRoutes);
-app.use("/categories", categoryRoutes);
-app.use("/audits", auditRoutes);
+app.use("/users", userRoutes)
+app.use("/reports", reportRoutes)
+app.use("/replies", replyRoutes)
+app.use("/search", searchRoutes)
+app.use("/priorities", priorityRoutes)
+app.use("/categories", categoryRoutes)
+app.use("/audits", auditRoutes)
 
 app.get("/", (req, res) => {
-  res.send("Hello, world!!!");
-});
+  res.send("Hello, world!!!")
+})
 
 app.listen(PORT, () => {
-  console.log(`We are aliiivee on port ${PORT}`);
-});
+  console.log(`We are aliiivee on port ${PORT}`)
+})
