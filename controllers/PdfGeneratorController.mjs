126bc4a5e30147c2554ba62ea70a7100b48e5ff3
latteflo <sf.simion.f@gmail.com>
2023-09-10 16:36:53 +0200

    completed the pdf generation function with db placeholders

--- controllers/PdfGeneratorController.mjs
+++ controllers/PdfGeneratorController.mjs
@@ -1,5 +1,8 @@
 import { generatePDFWithAPI } from "../config/pdfConfig.mjs"
 import { getReportByIdModel } from "../models/ReportModel.mjs"
+import {getCategoryByIdModel} from "../models/CategoryModel.mjs"
+import {getPriorityByIdModel} from "../models/PriorityModel.mjs"
+import {getUserByIdModel} from "../models/UserModel.mjs"
 
 export const generateReportPDF = async (req, res) => {
   try {
@@ -10,7 +13,29 @@ export const generateReportPDF = async (req, res) => {
       return res.status(404).json({ message: "Report not found" });
     }
 
-    const pdf = await generatePDFWithAPI(report);
+    // Fetch additional data based on their IDs
+    const category = await getCategoryByIdModel(report.category_id);
+    const priority = await getPriorityByIdModel(report.priority_id);
+    const user = report.is_anonymous ? null : await getUserByIdModel(report.user_id);
+
+    // Map database fields to template fields
+    const mappedReportData = {
+      ReportTitle: report.title,
+      ReportDescription: report.description,
+      IsAnonymous: report.is_anonymous ? "Yes" : "No",
+      InvolveOthers: report.involve_others ? "Yes" : "No",
+      ReportStatus: report.status,
+      SubmittedAt: new Date(report.submitted_at).toLocaleString(),
+      SubmittedBy: report.is_anonymous ? "Anonymous" : `${user.first_name} ${user.last_name}`,
+      ReportCategory: category ? category.name : "N/A",
+      ReportPriority: priority ? priority.name : "N/A",
+      ReportPriorityColor: priority ? priority.color_code : "N/A",
+      MediaURL: report.media,
+      GeneratedByAdminID: req.user.id,
+      GeneratedByAdminUsername: req.user.username,
+    };
+
+    const pdf = await generatePDFWithAPI(mappedReportData);
 
     if (pdf) {
       res.set({
