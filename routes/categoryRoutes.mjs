175e0a7dc71c9565bac745131490bacfb1118688
latteflo <sf.simion.f@gmail.com>
2023-09-12 12:20:59 +0200

    changed category route- removed admin auth

--- routes/categoryRoutes.mjs
+++ routes/categoryRoutes.mjs
@@ -7,11 +7,11 @@ import {
   updateCategory,
   deleteCategory,
 } from "../controllers/CategoryController.mjs"
-import { authMiddlewareWithRole } from "../middleware/authMiddleware.mjs"
+import { authMiddleware, authMiddlewareWithRole } from "../middleware/authMiddleware.mjs"
 const router = express.Router();
 
 router.post("/create", authMiddlewareWithRole("admin"), createCategory)
-router.get("/get-all", authMiddlewareWithRole("admin"), getAllCategories)
+router.get("/all", authMiddleware, getAllCategories)
 router.get("/:id", authMiddlewareWithRole("admin"), getCategoryById)
 router.get("/:id/reports",authMiddlewareWithRole("admin"),getReportsByCategoryId)
 router.put("/:id", authMiddlewareWithRole("admin"), updateCategory)
