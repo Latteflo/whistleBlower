d5b69287dbebdb360d67cd3b9a0302b6abea9c5f
latteflo <sf.simion.f@gmail.com>
2023-09-14 12:32:52 +0200

    changed model sql for reply

--- models/ReplyModel.mjs
+++ models/ReplyModel.mjs
@@ -1,6 +1,22 @@
 import { pool } from "../config/db.mjs"
 
-export const createReplyModel = async (reportId, userId, text) => {
+// Function to fetch user_role.id based on auth_id
+const fetchUserRoleById = async (authId) => {
+  const query = "SELECT id FROM user_role WHERE auth_id = $1";
+  try {
+    const result = await pool.query(query, [authId]);
+    return result.rows[0]?.id;  
+  } catch (err) {
+    console.error("Error in fetching user role ID:", err);
+    throw err;
+  }
+};
+
+// Function to create a reply
+export const createReplyModel = async (reportId, authId, text) => {
+  // Fetch the user_role.id based on auth_id
+  const userId = await fetchUserRoleById(authId);
+
   if (!userId) {
     console.error("User ID is null");
     throw new Error("User ID is null");
